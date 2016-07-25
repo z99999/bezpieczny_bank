20b2a05c5f41f3d2cdf6faed8712f150261b3503
zuzia <zuzia@MacBook-zuzia.local>
2016-07-25 18:33:12 +0200

    push

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", function(event){
         if(!(window.matchMedia("(min-width:550px)").matches)){
             if(!$(".clients_section").attr("isMobile")) {
                 $(".clients_section").attr("isMobile", "true");
-                $(".clients_section").find("li").css("display", "none");
+                $(".clients_section ul").css("display", "none");
                 $(".clients_section h6").on("click", function(event){
                     var $this = $(this);
                     $(this).siblings().toggle();
@@ -23,8 +23,8 @@ document.addEventListener("DOMContentLoaded", function(event){
         } else {
             if($(".clients_section").attr("isMobile")) {
                 $(".clients_section").removeAttr("isMobile");
-                $(".clients_section").find("li").css("display", "list-item");
-                $(".clients_section").find("h6").off("click");
+                $(".clients_section ul").css("display", "block");
+                $(".clients_section h6").off("click");
             }
         }
     };
@@ -51,10 +51,42 @@ document.addEventListener("DOMContentLoaded", function(event){
     }
    
     
+    //slider
+    
+    
+    calcValueToPay($(".czas").text(), $(".kwota").text().replace(' ', ''))
+    
+    $(".slider1").slider({
+        range:"max",
+        min:10000,
+        max:100000,
+        step:1000,
+        value:0,
+        slide: function(event, response) {
+            $(".kwota").text(thousandsSpace(response.value));
+            calcValueToPay($(".czas").text(), response.value);
+        }
+    });
         
+    $(".slider2").slider({
+        range:"max",
+        min:6,
+        max:24,
+        step:1,
+        value:24,
+        slide: function(event, response) {
+            $(".czas").text(response.value);
+            calcValueToPay(response.value, $(".kwota").text().replace(' ', ''));
+        }
+    });
     
+    function calcValueToPay(months, value) {
+        return $(".rata").text(thousandsSpace(Math.round(((value/months)*1.03).toFixed(2))));
+    }
     
-    
+    function thousandsSpace(x) {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+    }
     
 });
 
